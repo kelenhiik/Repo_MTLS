6dee136e6fe687ac9348b44acf201e6abf43cd24
kelehiik <helen.kiik@stud.ki.se>
2018-02-25 16:28:54 +0100

    managed to get empty placeholders to the sequence, but the sliding window is messed up now

--- sliding_window_try.py
+++ sliding_window_try.py
@@ -6,42 +6,49 @@ def fasta_parser(filename):
     return (dict1)
 
 def slidw1(seq,sw):
-    #sw1-sliding window positions after element in view
-    #sw2-sliding window positions before element in view
-    #sw1=(sw//2)
-    #sw2=-sw1
-    #print (sw2)
-    #sw=int(sw)
+    
+    
     window=[]
+    seqpos=[]
     all_windows=[]
     aa_list=[]
     aa_=[]
+    seq_ws=[]
    # print (seq)
     #prints dictionary
    # print ("-----")
     for ID in seq:
+        
        # print (ID)
         #prints the dictionary keys aka ID
        # print("llllllll")
         aa_list.append((seq[ID])[0])
-    print(aa_list)    
-    #prints the dictionary's first values, so ID value in position 0 
-   # for aa_element in aa_list:
-           # print (aa_value)
-           # aa_list.append(aa_value)
-   
-    pos_in_window=list(range(0,len(aa_list)))
-    #print(pos_in_window)#---[0, 1,] position of sequences in list
-    #seqkeys= seq.values[1]   
-    for element in aa_list:
-        #for positions in
-        print (element) #this just prints the sequences, because I guess it takes the element as the thing that fills the positions 0 and 1 that i have.... 
-       # window.append(element)
-    #print(window) #['A', 'D', 'E', 'R', 'R', 'G']
 
+    for aa in aa_list:
+       # print (aa)
+        seq_ws.append('0'*(sw//2)+aa+('0'*(sw//2)))
+   # print (seq_ws) #prints out the seqs with 0 added to the end and the beginning.
+   # print(aa_list)    
+   
+    for seqs in seq_ws:
+       # print (len( seqs)) # -prints lengths of sequences under each other. 
+        for position, aminoa in enumerate( seqs):
+           # print (position,aminoa)
+            seqpos.append( position) #I don't think this is a necessary thing here
+            window.append (aminoa)
+            if position+sw <= len(seqs):
+                all_windows.extend(window[position:(position+sw)])
+    print (all_windows)
+   # print (seqpos)
+    #pos_in_window=list(range(0,len(seq_ws)))
+   # print(pos_in_window)#---[0, 1,] position of sequences in list
+       
+   # for element in aa_list:
+        #for positions in
+       # print (element) #this just prints the sequences, because I guess it takes the element as the thing that fills the positions 0 and 1 that i have.... 
+     
     
-    
-    for number in pos_in_window: 
+   # for number in pos_in_window: 
         #print (number)
                         #0 
                         #1
@@ -50,10 +57,11 @@ def slidw1(seq,sw):
                         #4
                         #5
 
-        if sw+number <= len(seq):
+       # if sw+number <= len(seq):
         # if sliding window size and the number from pos_window sum up to be less or equal to the length of the sequence, so it wouldn't continue going over the sequence creating lists smaller than the window size. 
         
-            all_windows.extend (window[number:(number+sw)])
+          
+        #all_windows.extend (window[number:(number+sw)])
             #adds all the values in window list that correspond to the positions from and between number and sw+number (where it starts the value and goes one by one, so does the sliding window shift as much) into one continous list.
          
     
