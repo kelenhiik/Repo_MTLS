72ea2ed92996109c8d943eafd986f128814195ea
kelehiik <helen.kiik@stud.ki.se>
2018-03-01 12:13:04 +0100

    a few mods to all parsing codes and training

--- 8state_predictor/bin/all_parsing_codes.py
+++ 8state_predictor/bin/all_parsing_codes.py
@@ -1,7 +1,10 @@
 import numpy as np
 
+################################################################################
+# This function takes a filename and a sliding window and returns all sequences in binary with appropriate sliding windows. The format for SVM predicting?
+################################################################################
 
-#### This function takes a filename and a sliding window and returns all sequences in binary with appropriate sliding windows. The format for SVM predicting?###############
+
 def parse_unknown_file(filename, sliding_window):
     all_unknown_topo_seq_set = []
 
@@ -15,8 +18,11 @@ def parse_unknown_file(filename, sliding_window):
 
     all_unknown_topo_seq_set = np.array(all_unknown_topo_seq_set)
     return all_unknown_topo_seq_set
-     
-#### This function takes a filename and a sliding window and returns all sequences in binary with appropriate sliding windows. The format for SVM training.###############
+    
+    
+################################################################################
+# This function takes a filename and a sliding window and returns all sequences in binary with appropriate sliding windows. The format for SVM training.
+################################################################################
 
 def parse_with_all_codes(filename, sliding_window):
     all_training_set = []
@@ -32,8 +38,12 @@ def parse_with_all_codes(filename, sliding_window):
     all_topologies = np.array(all_topologies)
     all_training_set = np.array(all_training_set)
     return all_training_set, all_topologies
-    
-#### This function takes a filename and a sliding window. Then it calls the parser function and makes a dictionary out of the filename. Afterwards it splits the dictionary keys so I could have a separate training and testing set. Complicated with a dictionary, bias is minimized by sorting the dictionary keys by the positions in the name of the key that have been assigned randomly in pdb. The out of this key list it takes 70% for training and results in 30% for testing sets that it encodes for proteins and topology using custom functions, returns results as numpy arrays.####
+ 
+################################################################################    
+# This function takes a filename and a sliding window. Then it calls the parser function and makes a dictionary out of the filename. Afterwards it splits the dictionary keys so I could have a separate training and testing set. Complicated with a dictionary, bias is minimized by sorting the dictionary keys by the positions in the name of the key that have been assigned randomly in pdb. The out of this key list it takes 70% for training and results in 30% for testing sets that it encodes for proteins and topology using custom functions, returns results as numpy arrays.
+################################################################################
+
+
 def parse_with_train_test(filename, sliding_window):
     all_training_set = []
     train_topologies = []
@@ -69,8 +79,13 @@ def parse_with_train_test(filename, sliding_window):
     test_topologies = np.array(test_topologies)
     
     return all_training_set, train_topologies, all_test_set, test_topologies
+    
+    
+################################################################################
+# Takes a file with ID, seq, topology and assigns ID as key, seq and topology as values.
+################################################################################
 
-##### Takes a file with ID, seq, topology and assigns ID as key, seq and topology as values.######
+
 def fasta_parser(filename):
     list1=[line.upper().rstrip() for line in (open (filename, 'r')) if len (line.strip()) != 0]
     
@@ -85,11 +100,19 @@ def fasta_parser_onlyseq(filename):
     return (dict2)
 
 
+################################################################################
+# Manually made dictionary for the topologies in numerical form
+################################################################################
 
-### Manually made dictionary for the topologies in numerical form###
+
 topology_dict={'G':1, 'I':2, 'H':3, 'E':4, 'B':5, 'T':6, 'S':7, 'C':8}
 
-### Create a binary dictionary for the amino acid residues and specify what to do with uncommon residues ###
+
+################################################################################
+### Create a binary dictionary for the amino acid residues and specify what to do with uncommon residues 
+################################################################################
+
+
 amino_acid_dict={}
 aminoacids='GALMFWKQESPVICYHRNDT'
 for aa in aminoacids:
@@ -117,8 +140,11 @@ amino_acid_dict['O'] = amino_acid_dict['S']
 amino_acid_dict['U'] = amino_acid_dict['S']
 
 
+################################################################################
+# Go through a sequence and do all full sliding windows#
+################################################################################
 
-##### Go through a sequence and do all full sliding windows###
+
 def encode_protein(seq, windowlength):
     training_set = []
     pad=int(windowlength//2)
@@ -147,9 +173,10 @@ def encode_protein(seq, windowlength):
             training_set.append(temp_encoded_window) # Save to training set
     return training_set
 
-###########################
+################################################################################
 # Fix topologies #
-#######################    
+################################################################################
+    
 
 def topology_in_numbers(my_topo):
     topologies=[]
@@ -158,6 +185,7 @@ def topology_in_numbers(my_topo):
         topologies.append(topology_dict[topology])
     
     return topologies
+    
 if __name__ == "__main__":
     #print(parse_with_all_codes("dssp_8_state.3line.txt", 3))
     print(fasta_parser_onlyseq('250_270_set.3line.txt'))
--- 8state_predictor/bin/training.py
+++ 8state_predictor/bin/training.py
@@ -1,23 +1,34 @@
 import all_parsing_codes
 from sklearn import svm
 
+
 #X,Y=(all_parsing_codes.parse_with_all_codes('100_proteins.3line.txt',11))
-unknown=(all_parsing_codes.parse_unknown_file('250_270_set.3line.txt', 11))
+#unknown=(all_parsing_codes.parse_unknown_file('250_270_set.3line.txt', 11))
 #clf = svm.SVC(kernel='linear') #model
 #clf.fit(X, Y) 
 #score=clf.score(X,Y)
 #print(score)
-
-
-X_train, Y_train, X_test, Y_test = all_parsing_codes.parse_with_train_test('100_proteins.3line.txt', 11)
-
+topology_dict={1:'G', 2:'I', 3:'H', 4:'E', 5:'B', 6:'T', 7:'S', 8:'C'}
+##########################################################################################
+#Split my dataset into 70% and 30%. 70% being the training set and 30% being the test set#
+##########################################################################################
+X_train, Y_train, X_test, Y_test = all_parsing_codes.parse_with_train_test('34_proteins.3line.txt', 11)
+#'../data/train_test_sets/34_proteins.3line.txt' - does not work, [Errno 20] not a directory... but it is...
+#fit the model with the training set#
 clf = svm.SVC(kernel='linear', cache_size=3000)
 clf.fit(X_train, Y_train) 
-
+################################################
+#use the testing set to see if predicting works#
+################################################
 prediction=clf.predict(X_test)
+prediction_states=prediction.tolist()
+list_of_ss=[]
+for number in prediction_states:
+
+    list_of_ss.extend(topology_dict[number])
 
 
-print('linear',  prediction )
+print(list_of_ss)
 
 
 #for kernel in ('linear', 'rbf', 'polinomial'):
