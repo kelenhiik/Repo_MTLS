7b1f066b3e946f7d5d4bc1acd1ed78c87f3a58a7
kelehiik <helen.kiik@stud.ki.se>
2018-03-07 16:53:16 +0100

    Able to take in a file and predict for every protein separately

--- 8state_predictor/bin/train_SVM_oneprotein_predict.py
+++ 8state_predictor/bin/train_SVM_oneprotein_predict.py
@@ -5,9 +5,9 @@ import all_parsing_codes
 import numpy as np
 from sklearn import svm
 from sklearn.model_selection import cross_val_score
-tempfile = './34_proteins.3line.txt'
+tempfile = './11_proteins.txt'
 #tempfile = '../data/train_test_sets/34_proteins.3line.txt'
-unknown = './oneseq.txt'
+unknown = './twoseq.txt'
 
 
 ##########################################################################################
@@ -25,37 +25,33 @@ X_train, Y_train, X_test, Y_test = all_parsing_codes.parse_with_train_test(tempf
 clf = svm.SVC(kernel='linear', cache_size=3000)
 clf.fit(X_train, Y_train)
 
-#########################################################################
-#use a hardcoded sequence for predicting#
-#########################################################################
-#Notice, not using the test set anywhere at the moment, since I want to see how it works for one sequence.#
 
-Test, id_of_test, seq_of_test = all_parsing_codes.parse_unknown_file(unknown, 11)
 
-prediction=clf.predict(Test)
+sliding_window = 11
 
-#############################################################################
-#Retranslate the output to secondary structure elements as described by DSSP#
-#############################################################################
+# This takes every sequence in a dicionary one at a time, predicts and then goes to the next one, also creates a file for it #
 
 topology_dict={1:'G', 2:'I', 3:'H', 4:'E', 5:'B', 6:'T', 7:'S', 8:'C'}
-
-prediction_states=prediction.tolist()
-list_of_ss=[]
-
-
-for number in prediction_states:
-
-    list_of_ss.extend(topology_dict[number])
-
-
-list_in_string="".join(list_of_ss)
-
-
 output=open("prediction2.txt",'w')
-output.write(id_of_test + '\n' + seq_of_test + '\n' + list_in_string)
-output.close()
+dictionary = all_parsing_codes.fasta_parsing_from_3lines(unknown)
+for identification in dictionary:
+    print(identification)
+    
+    sw_of_unknown_topo_seq = all_parsing_codes.encode_protein((dictionary[identification]), sliding_window)
+    sw_of_unknown_topo_seq = np.array(sw_of_unknown_topo_seq)
+    prediction=clf.predict(sw_of_unknown_topo_seq)
+    prediction_states=prediction.tolist()
+    list_of_ss=[]
 
+    for number in prediction_states:
+
+        list_of_ss.extend(topology_dict[number])
+
+
+    list_in_string="".join(list_of_ss)
+
+    output.write(identification + '\n' + dictionary[identification] + '\n' + list_in_string + '\n')
+output.close()
 
 
 
