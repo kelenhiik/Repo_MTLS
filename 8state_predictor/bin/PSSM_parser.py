3897e36bfc4a3dbf6297c2ab63acaec8492570e5
kelehiik <helen.kiik@stud.ki.se>
2018-03-10 21:55:02 +0100

    I think this works?!

--- 8state_predictor/bin/PSSM_parser.py
+++ 8state_predictor/bin/PSSM_parser.py
@@ -1,23 +1,27 @@
 """ Parses PSSM profiles for machine learning """
 import re
-
+import numpy as np
 def pssm_parsing(filename):
     """ Sliding windows for PSSM profile """
-    profile = open(filename, 'r')
+    info = (np.genfromtxt(filename, skip_header = 3, skip_footer = 5, autostrip = True, usecols = range(22,42)))/100
+
+    print (info)
+
+
+    """profile = open(filename, 'r')
     profile2=profile.readlines()
     for position in (profile2[2]):
         if position == ' ':
             pass
         else:
             print(position)
-        
-    
+
+
     #print (profile)
     #re.compile[" + \d + [A-Z]"] #Used to indicate a set of characters, separated by "-"
-    profile.close()
+    profile.close()"""
 
 
 
 if __name__ == "__main__":
     print(pssm_parsing(">D1A04.fasta.pssm"))
-    
