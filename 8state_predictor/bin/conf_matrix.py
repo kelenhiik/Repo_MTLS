2e933856a48be3de0f67c149334455a63cd77fc3
kelehiik <helen.kiik@stud.ki.se>
2018-03-19 10:45:51 +0100

    some final metrics

--- 8state_predictor/bin/conf_matrix.py
+++ 8state_predictor/bin/conf_matrix.py
@@ -15,8 +15,8 @@ TEMPFILE = '../data/train_test_sets/randomized109_proteins.3line.txt'
 TEMPFILE2 = '../data/testing_sets/dataset_of_50.txt'
 win_len = 7
 class_names = ['G', 'I', 'H', 'E', 'B', 'T', 'S', 'C']
-X_train, y_train = all_parsing_codes.pssm_svm(TEMPFILE, win_len)
-X_test, y_test = all_parsing_codes.pssm_svm(TEMPFILE2, win_len)
+X_train, y_train, e, o = all_parsing_codes.parse_with_train_test(TEMPFILE, win_len)
+X_test, y_test = all_parsing_codes.parse_with_all_codes(TEMPFILE2, win_len)
 #X_train, y_train, X_test, y_test = all_parsing_codes.protein_w_pssm_train(TEMPFILE,win_len)
 
 classifier_model = RFC(n_estimators = 350, min_samples_split = 2, n_jobs = -1)
@@ -63,11 +63,11 @@ np.set_printoptions(precision=2)
 # Plot non-normalized confusion matrix
 plt.figure()
 plot_confusion_matrix(cnf_matrix, classes=class_names,
-                      title='Non-normalized confusion matrix for balanced PSFM Random forests on external dataset')
-plt.savefig('./extprot_non_norm_psfm.png')
+                      title='Non-normalized confusion matrix for PSFM Random forests on external dataset')
+plt.savefig('./extprot_non_norm.png')
 # Plot normalized confusion matrix
 plt.figure()
 plot_confusion_matrix(cnf_matrix, classes=class_names, normalize=True,
-                      title='Normalized confusion matrix for balanced PSFM Random forests on external dataset')
-plt.savefig('./extprot_norm_psfm.png')
+                      title='Normalized confusion matrix for PSFM Random forests on external dataset')
+plt.savefig('./extprot_norm.png')
 plt.show()
--- 8state_predictor/bin/randomforestclassifier.py
+++ 8state_predictor/bin/randomforestclassifier.py
@@ -8,43 +8,40 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 import all_parsing_codes
 
-TEMPFILE = '../data/train_test_sets/randomized109_proteins.3line.txt'
-OUTPUT = open("../results/testing_results/RFC_metrics.txt", 'w')
-OUTPUT2 = open("../results/testing_results/RFC_crossvalidation.txt", 'w')
+tempfile = '../data/train_test_sets/randomized109_proteins.3line.txt'
+TEMPFILE2 = '../data/testing_sets/dataset_of_50.txt'
+OUTPUT = open("../results/testing_results/RFC_metrics_again.txt", 'w')
 
 
-for n_estimators1 in (10, 100, 200, 300, 350):
-    for win_len in range(5,22,2):
-        for min_samples_split1 in range(2,9):
-            X_TRAIN, Y_TRAIN, X_TEST, Y_TEST = all_parsing_codes.parse_with_train_test(TEMPFILE, 
-                                                                                       win_len)
-            MODEL = RFC(n_estimators=n_estimators1, min_samples_split = min_samples_split1, 
-                        n_jobs=-1)
-            MODEL.fit(X_TRAIN, Y_TRAIN,)
-            PREDICTION = MODEL.predict(X_TEST)
-            REPORT = classification_report(Y_TEST, PREDICTION, labels=[1, 2, 3, 4, 5, 6, 7, 8], 
-                                           target_names=['G', 'I', 'H', 'E', 'B', 'T', 'S', 'C'])
-            CONFUSION = confusion_matrix(Y_TEST, PREDICTION, labels=[1, 2, 3, 4, 5, 6, 7, 8])
-            MATTHEW = matthews_corrcoef(Y_TEST, PREDICTION)
-            print (type(CONFUSION))
+X_TRAIN, Y_TRAIN, X_TEST, Y_TEST = all_parsing_codes.parse_with_train_test(tempfile, 11)
+
+MODEL = RFC(n_estimators=350, min_samples_split = 3, 
+            n_jobs=-1)
+MODEL.fit(X_TRAIN, Y_TRAIN,)
+PREDICTION = MODEL.predict(X_TEST)
+REPORT = classification_report(Y_TEST, PREDICTION, labels=[1, 2, 3, 4, 5, 6, 7, 8], 
+                               target_names=['G', 'I', 'H', 'E', 'B', 'T', 'S', 'C'])
+CONFUSION = confusion_matrix(Y_TEST, PREDICTION, labels=[1, 2, 3, 4, 5, 6, 7, 8])
+MATTHEW = matthews_corrcoef(Y_TEST, PREDICTION)
+#print (type(CONFUSION))
             
-            OUTPUT.write('Window length: ' + '\n' + str(win_len) + '\n' +
-                         'N-estimators: ' + '\n' + str(n_estimators1) + '\n' +
-                         'Min_samples_split: ' + '\n' + str(min_samples_split1) + '\n' +
-                         'Classification report: ' + '\n' + str(REPORT) + '\n' +
-                         'Confusion matrix: ' + '\n' + str(CONFUSION) + '\n' +
-                         'Matthews correlation: ' + '\n' + str(MATTHEW) + '\n')
-            print('Window length: ' + str(win_len),
-                         'N-estimators: ' + str(n_estimators1),
-                         'Min_samples_split: ' + str(min_samples_split1),
-                         'Classification report: ' + str(REPORT),
-                         'Confusion matrix: ' + str(CONFUSION),
-                         'Matthews correlation: ' +  str(MATTHEW))
+OUTPUT.write('Window length: ' + '\n' + str(11) + '\n' +
+             'N-estimators: ' + '\n' + str(350) + '\n' +
+             'Min_samples_split: ' + '\n' + str(3) + '\n' +
+             'Classification report: ' + '\n' + str(REPORT) + '\n' +
+             'Confusion matrix: ' + '\n' + str(CONFUSION) + '\n' +
+             'Matthews correlation: ' + '\n' + str(MATTHEW) + '\n')
+print('Window length: ' + str(11),
+      'N-estimators: ' + str(350),
+      'Min_samples_split: ' + str(3),
+      'Classification report: ' + str(REPORT),
+      'Confusion matrix: ' + str(CONFUSION),
+      'Matthews correlation: ' +  str(MATTHEW))
       
                  
+
 OUTPUT.close()
-
-
+"""
 
 TOPOLOGY_DICT = {'G':1, 'I':2, 'H':3, 'E':4, 'B':5, 'T':6, 'S':7, 'C':8}
 
@@ -64,4 +61,4 @@ for n_estimators1 in (10, 100, 200, 300, 350):
                           " cross-validation score: " + str(SCORE_AVERAGE), " standard deviation: " + str(SCORE_DEVIATION))
             
         
-OUTPUT2.close()
+OUTPUT2.close()"""
